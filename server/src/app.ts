de0d1a6674db089464c3d0f578fc396fadc34b94
Neethu <neethu.jacob@shadline.com>
2025-05-29 16:43:31 +0200

    feat(docs): add Swagger/OpenAPI documentation setup
    - Installed swagger-jsdoc and swagger-ui-express
    - Created swagger.ts to initialize Swagger UI at /api-docs
    - Added swagger-jsdoc.d.ts for type support

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth";
 import recipeRoutes from "./routes/recipe";
+import { setupSwagger } from "./swagger";
 
 dotenv.config();
 
@@ -14,5 +15,7 @@ app.use("/api/recipes", recipeRoutes);
 app.get("/", (req, res) => {
   res.send("Welcome to the Recipe Organizer!");
 });
+
+setupSwagger(app);
 
 export default app;
